396ef9ebb9331e1897abad5aa5c1f6e09ff3d1ed
redhocode <redhoarifin2017@gmail.com>
2024-05-18 12:25:28 +0900

    add middleware

--- src/apps/user/auth.router.ts
+++ src/apps/user/auth.router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { registerUser, createSession, refreshToken, getAll, updateUser, deleteUser, logout } from './auth.contoller'
+import { requireAdmin } from '../../middleware/auth'
+
 
 export const AuthRouter: Router = Router()
 
-AuthRouter.post('/register', registerUser)
+AuthRouter.post('/register',registerUser)
 AuthRouter.post('/login', createSession)
 AuthRouter.post('/refresh', refreshToken)
-AuthRouter.get('/', getAll)
+AuthRouter.get('/',requireAdmin,getAll)
 AuthRouter.patch('/:id', updateUser)
 AuthRouter.delete('/:id', deleteUser)
 AuthRouter.post('/logout', logout)
